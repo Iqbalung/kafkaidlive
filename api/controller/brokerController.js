08d18dafdd122c8bf3c371d0ad624f2fdf05f65a
fadholifh <fadholifh14@gmail.com>
2022-02-16 17:29:58 +0700

    status
    status

--- api/controller/brokerController.js
+++ api/controller/brokerController.js
@@ -120,10 +120,26 @@ class BrokerController {
 
     axios(config)
       .then((response) => {
-        console.log(response.status)
+        console.log('status', response.status)
+        console.log('data ',response.status.data)
       })
       .catch((error) => {
-        console.log(error)
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log('response ', error.response.data)
+          console.log('response ', error.response.status)
+          console.log('response ', error.response.headers)
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log('request ',error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message)
+        }
+        console.log(error.config)
       })
   }
 }
